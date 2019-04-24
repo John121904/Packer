cd637c73aa9af4d1ac1daf5d04e74df98754d7af
cfredericksen <chad.fredericksen@carlsonwagonlit.com>
2019-04-24 10:58:13 -0500

    folder changed

--- nutanix-temp/scripts/shutdown.bat
+++
@@ -1,2 +0,0 @@
-powershell "c:/windows/temp/configure-ansible.ps1"
-shutdown /s /t 10 /f /d p:4:1 /c "Packer Shutdown"
--- vmware-temp/scripts/changeCDdrive.ps1
+++
@@ -1,4 +0,0 @@
-#Change CD drive letter
-$drv = Get-WmiObject win32_volume -filter 'DriveLetter = "D:"'
-$drv.DriveLetter = "F:"
-$drv.Put() | out-null
