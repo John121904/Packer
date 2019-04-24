ca9202677fa5dd58d16728b4da9269fc88d72321
cfredericksen <chad.fredericksen@carlsonwagonlit.com>
2019-04-24 10:49:30 -0500

    changed packer folder structure

--- scripts/compact.ps1
+++
@@ -1,4 +0,0 @@
-#Run SDELETE on the C drive
-cmd /c Dism /online /cleanup-image /startcomponentcleanup 
-cmd /c %SystemRoot%\System32\reg.exe ADD HKCU\Software\Sysinternals\SDelete /v EulaAccepted /t REG_DWORD /d 1 /f
-cmd /c A:\sdelete.exe -q -z C:
